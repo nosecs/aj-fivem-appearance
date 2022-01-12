44516fdbb9e826f978cf13eb51d04e789f9e1c2d
Tony <1414927+zfbx@users.noreply.github.com>
2022-01-11 20:44:33 -0500

    convert to oxmysql v1.9.0

--- server/server.lua
+++ server/server.lua
@@ -4,7 +4,7 @@ local QBCore = exports['qb-core']:GetCoreObject()
 
 QBCore.Functions.CreateCallback('fivem-appearance:getPlayerSkin', function(source, cb)
 	local Player = QBCore.Functions.GetPlayer(source)
-	local players = exports.oxmysql:fetchSync('SELECT skin FROM players WHERE citizenid = ?', {Player.PlayerData.citizenid})
+	local players = MySQL.Sync.execute('SELECT skin FROM players WHERE citizenid = ?', {Player.PlayerData.citizenid})
 	local player, appearance = players[1]
 	if player.skin then
 		appearance = json.decode(player.skin)
@@ -16,18 +16,18 @@ end)
 
 RegisterNetEvent('fivem-appearance:save', function(appearance)
 	local Player = QBCore.Functions.GetPlayer(source)
-	exports.oxmysql:execute('UPDATE players SET skin = ? WHERE citizenid = ?', {json.encode(appearance), Player.PlayerData.citizenid})
+	MySQL.query('UPDATE players SET skin = ? WHERE citizenid = ?', {json.encode(appearance), Player.PlayerData.citizenid})
 end)
 
 RegisterNetEvent('fivem-appearance:saveOutfit', function(name, pedModel, pedComponents, pedProps)
 	local Player = QBCore.Functions.GetPlayer(source)
-	exports.oxmysql:insert('INSERT INTO `player_outfits` (citizenid, name, ped, components, props) VALUES (?, ?, ?, ?, ?)', {Player.PlayerData.citizenid, name, json.encode(pedModel), json.encode(pedComponents), json.encode(pedProps)})
+	MySQL.insert('INSERT INTO `player_outfits` (citizenid, name, ped, components, props) VALUES (?, ?, ?, ?, ?)', {Player.PlayerData.citizenid, name, json.encode(pedModel), json.encode(pedComponents), json.encode(pedProps)})
 end)
 
 RegisterNetEvent('fivem-appearance:getOutfit', function(name)
 	local src = source
 	local Player = QBCore.Functions.GetPlayer(src)
-	local outfit = exports.oxmysql:fetchSync('SELECT outfit FROM player_outfits WHERE citizenid = @citizenid AND name = @name', {['@citizenid'] = Player.PlayerData.citizenid, ['@name'] = name})
+	local outfit = MySQL.Sync.execute('SELECT outfit FROM player_outfits WHERE citizenid = @citizenid AND name = @name', {['@citizenid'] = Player.PlayerData.citizenid, ['@name'] = name})
 	local newOutfit = outfit
 	if newOutfit then
 		newOutfit = json.decode(newOutfit)
@@ -39,7 +39,7 @@ RegisterNetEvent('fivem-appearance:getOutfits', function()
 	local src = source
 	local Player = QBCore.Functions.GetPlayer(src)
 	local myOutfits = {}
-	local result = exports.oxmysql:fetchSync('SELECT id, name, ped, components, props FROM player_outfits WHERE citizenid = ?', {Player.PlayerData.citizenid})
+	local result = MySQL.Sync.execute('SELECT id, name, ped, components, props FROM player_outfits WHERE citizenid = ?', {Player.PlayerData.citizenid})
 	for i=1, #result, 1 do
 		table.insert(myOutfits, {id = result[i].id, name = result[i].name, ped = json.decode(result[i].ped), components = json.decode(result[i].components), props = json.decode(result[i].props)})
 	end
@@ -48,7 +48,7 @@ end)
 
 RegisterNetEvent('fivem-appearance:deleteOutfit', function(id)
 	local Player = QBCore.Functions.GetPlayer(source)
-	exports.oxmysql:execute('DELETE FROM player_outfits WHERE id = ?', {id})
+	MySQL.query('DELETE FROM player_outfits WHERE id = ?', {id})
 end)
 
 RegisterNetEvent('fivem-appearances:buyclothing', function()
